a34afbda888ff06c4a8c19e32150156ed7fe6936
= <=>
2023-08-14 17:43:55 +0700

    ganti nama

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Master\PenyewaController;
+use App\Http\Controllers\Api\Master\SopirController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -28,10 +29,10 @@ Route::group(['prefix' => 'master'],function(){
         Route::delete("/{id}", [PenyewaController::class, "destroy"]);
     });
     Route::group(['prefix' =>"sopir"],function(){
-        Route::get("/", [PenyewaController::class, "index"]);
-        Route::post("/", [PenyewaController::class, "store"]);
-        Route::get("/{id}", [PenyewaController::class, "show"]);
-        Route::put("/{id}", [PenyewaController::class, "update"]);
-        Route::delete("/{id}", [PenyewaController::class, "destroy"]);
+        Route::get("/", [SopirController::class, "index"]);
+        Route::post("/", [SopirController::class, "store"]);
+        Route::get("/{id}", [SopirController::class, "show"]);
+        Route::put("/{id}", [SopirController::class, "update"]);
+        Route::delete("/{id}", [SopirController::class, "destroy"]);
     });
 });
