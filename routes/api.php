78262038599f836793bb0f6d0997dd728d52394c
ferdinandanggris <ferdinandanggris@gmail.com>
2023-08-19 20:07:50 +0700

    api login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\Master\ArmadaController;
 use App\Http\Controllers\Api\Master\PenyewaController;
 use App\Http\Controllers\Api\Master\SopirController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post("/login",[AuthController::class,"login"]);
 
 Route::group(['prefix' => 'master'],function(){
     Route::group(['prefix' =>"penyewa"],function(){
