adce2471ca792089825184c4b549a46a5f045cf7
Farhan izzaz <farhanizzaz@gmail.com>
2023-08-24 20:40:53 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -69,13 +69,13 @@ Route::middleware('jwt.verify')->group(function() {
             Route::delete("/{id}", [SubkonController::class, "destroy"]);
         });
     });
+    Route::group(['prefix' => 'transaksi'], function () {
+        Route::group(['prefix' => 'hutang_sopir'], function () {
+            Route::get("/", [HutangSopirController::class, "index"]);
+            Route::post("/", [HutangSopirController::class, "store"]);
+            Route::get("/{id}", [HutangSopirController::class, "show"]);
+            Route::put("/{id}", [HutangSopirController::class, "update"]);
+            Route::delete("/{id}", [HutangSopirController::class, "destroy"]);
+        });
 });
-Route::group(['prefix' => 'transaksi'], function () {
-    Route::group(['prefix' => 'hutang_sopir'], function () {
-        Route::get("/", [HutangSopirController::class, "index"]);
-        Route::post("/", [HutangSopirController::class, "store"]);
-        Route::get("/{id}", [HutangSopirController::class, "show"]);
-        Route::put("/{id}", [HutangSopirController::class, "update"]);
-        Route::delete("/{id}", [HutangSopirController::class, "destroy"]);
-    });
 });
