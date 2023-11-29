45ce835bdba105b5e09c76330a969e7f0683fa2d
Ferdinand Anggris W <ferdinandanggris@gmail.com>
2023-11-30 00:36:24 +0700

    request order

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt.verify' => \App\Http\Middleware\JWTMiddleware::class,
+        'role' => \App\Http\Middleware\CheckRoles::class,
     ];
 }
