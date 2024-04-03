3c5f6a99e8104a09fcc9fc1347a7b81c0a81b368
Farhan izzaz <farhanizzaz@gmail.com>
2024-04-03 08:01:15 +0700

    Update MutasiModel.php

--- app/Models/Master/MutasiModel.php
+++ app/Models/Master/MutasiModel.php
@@ -65,7 +65,7 @@ class MutasiModel extends Model
         $item['detail'] = [
             'no_transaksi' => $dataOrder->no_transaksi,
             'harga_order' => $dataOrder->harga_order,
-            'biasa_lain_harga_order_arr' => $dataOrder->biaya_lain_harga_order_arr,
+            'biaya_lain_harga_order_arr' => $dataOrder->biaya_lain_harga_order_arr,
             'sisa_tagihan' => $dataOrder->sisa_tagihan,
             'muatan' => $dataOrder->muatan,
             'asal'  => $dataOrder->asal,
