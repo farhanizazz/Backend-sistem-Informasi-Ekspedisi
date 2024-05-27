fba8fd42fb8f53a7d10fd39a2bc4c8d905bbd4d5
Ferdinand Anggris W <ferdinandanggris@gmail.com>
2024-05-27 12:37:57 +0700

    servis response tambah rekening

--- app/Http/Controllers/Api/Transaksi/ServisController.php
+++ app/Http/Controllers/Api/Transaksi/ServisController.php
@@ -110,7 +110,7 @@ class ServisController extends Controller
     public function show($id)
     {
         try {
-            $result =  $this->servisModel->with('nota_beli_items.mutasi','master_armada')-> findOrFail($id);
+            $result =  $this->servisModel->with('nota_beli_items.mutasi.master_rekening','master_armada')-> findOrFail($id);
             return response()->json([
                 'status' => 'success',
                 'data' => new ServisResource($result)
