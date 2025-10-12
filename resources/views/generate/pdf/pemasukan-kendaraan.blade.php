b4b5b61d224ddb29a8f7369a4725e1d6c0bcf0da
Farhan izzaz <farhanizzaz@gmail.com>
2025-10-12 18:35:15 +0700

    fixed some bugs

--- resources/views/generate/pdf/pemasukan-kendaraan.blade.php
+++ resources/views/generate/pdf/pemasukan-kendaraan.blade.php
@@ -1,78 +1,94 @@
 <?php
-$total = 0; 
+$total = 0;
 
-function penyebut($nilai) {
+function penyebut($nilai)
+{
     $nilai = abs($nilai);
-    $huruf = array("", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
-    $temp = "";
+    $huruf = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas'];
+    $temp = '';
     if ($nilai < 12) {
-        $temp = " ". $huruf[$nilai];
-    } else if ($nilai <20) {
-        $temp = penyebut($nilai - 10). " belas";
-    } else if ($nilai < 100) {
-        $temp = penyebut($nilai/10)." puluh". penyebut($nilai % 10);
-    } else if ($nilai < 200) {
-        $temp = " seratus" . penyebut($nilai - 100);
-    } else if ($nilai < 1000) {
-        $temp = penyebut($nilai/100) . " ratus" . penyebut($nilai % 100);
-    } else if ($nilai < 2000) {
-        $temp = " seribu" . penyebut($nilai - 1000);
-    } else if ($nilai < 1000000) {
-        $temp = penyebut($nilai/1000) . " ribu" . penyebut($nilai % 1000);
-    } else if ($nilai < 1000000000) {
-        $temp = penyebut($nilai/1000000) . " juta" . penyebut($nilai % 1000000);
-    } else if ($nilai < 1000000000000) {
-        $temp = penyebut($nilai/1000000000) . " milyar" . penyebut(fmod($nilai,1000000000));
-    } else if ($nilai < 1000000000000000) {
-        $temp = penyebut($nilai/1000000000000) . " trilyun" . penyebut(fmod($nilai,1000000000000));
-    }     
+        $temp = ' ' . $huruf[$nilai];
+    } elseif ($nilai < 20) {
+        $temp = penyebut($nilai - 10) . ' belas';
+    } elseif ($nilai < 100) {
+        $temp = penyebut($nilai / 10) . ' puluh' . penyebut($nilai % 10);
+    } elseif ($nilai < 200) {
+        $temp = ' seratus' . penyebut($nilai - 100);
+    } elseif ($nilai < 1000) {
+        $temp = penyebut($nilai / 100) . ' ratus' . penyebut($nilai % 100);
+    } elseif ($nilai < 2000) {
+        $temp = ' seribu' . penyebut($nilai - 1000);
+    } elseif ($nilai < 1000000) {
+        $temp = penyebut($nilai / 1000) . ' ribu' . penyebut($nilai % 1000);
+    } elseif ($nilai < 1000000000) {
+        $temp = penyebut($nilai / 1000000) . ' juta' . penyebut($nilai % 1000000);
+    } elseif ($nilai < 1000000000000) {
+        $temp = penyebut($nilai / 1000000000) . ' milyar' . penyebut(fmod($nilai, 1000000000));
+    } elseif ($nilai < 1000000000000000) {
+        $temp = penyebut($nilai / 1000000000000) . ' trilyun' . penyebut(fmod($nilai, 1000000000000));
+    }
     return $temp;
 }
 ?>
 
 @extends('generate.pdf.layouts.main', ['title' => $filename])
 @section('container')
-
-<style>
-      html {
+    <style>
+        html {
             margin-top: 10;
         }
-</style>
+    </style>
 
-<h3 style="text-align: center;margin-bottom:0px">{{$title}}</h3>
-<p style="margin-top: 0px;text-align:center;font-size:.8rem">
-  ({{$periode}})
-</p>
-<div class="" style="margin-top: 1rem;margin-left:1rem;margin-right:1rem;">
-    <table border="1" style="border: solid 1px black;border-collapse: collapse;">
-        <thead style="margin-top: 1rem">
-            <tr  style="font-size:.9rem">
-                <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">No</th>
-                <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Tgl</th>
-                <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">No Transaksi</th>
-                <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Status</th>
-                <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Nopol / Sopir</th>
-                <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Penyewa</th>
-                <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Muatan</th>
-                <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Asal - Tujuan</th>
-                <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Setor</th>
-            </tr>
-        </thead>
-        <tbody>
-          @foreach($data as $dt)
-              <tr style="font-size:.7rem">
-                  <td style="text-align : center;">{{$loop->iteration}}</td>
-                  <td>{{date('d/m/Y', strtotime($dt['tanggal']))}}</td>
-                  <td style="text-align: center;">{{$dt['no_transaksi']}}</td>
-                  <td style="text-align: center;">{{ucwords($dt['status'])}}</td>
-                  <td style="text-align: center;">{{($dt['armada']['nopol'] ?? $dt['nopol_subkon'])}} / {{($dt['sopir']['nama'] ?? $dt['sopir_subkon'])}}</td>
-                  <td>{{($dt['penyewa']['nama_perusahaan'] ?? $dt['subkon']['nama_perusahaan']) ?? '' }}</td>
-                  <td>{{$dt['muatan']}}</td>
-                  <td>{{$dt['asal']}} - {{$dt['tujuan']}}</td>
-                  <td>{{rupiah($dt['setor'])}}</td>
-              </tr>
-          @endforeach
-      </tbody>
-    </table>
-</div>
+    <h3 style="text-align: center;margin-bottom:0px">{{ $title }}</h3>
+    <p style="margin-top: 0px;text-align:center;font-size:.8rem">
+        ({{ $periode }})
+    </p>
+    <div class="" style="margin-top: 1rem;margin-left:1rem;margin-right:1rem;">
+        <table border="1" style="border: solid 1px black;border-collapse: collapse;">
+            <thead style="margin-top: 1rem">
+                <tr style="font-size:.9rem">
+                    <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">No</th>
+                    <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Tgl</th>
+                    <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">No Transaksi</th>
+                    <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Status</th>
+                    <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Nopol / Sopir</th>
+                    <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Penyewa</th>
+                    <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Muatan</th>
+                    <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Asal - Tujuan</th>
+                    <th style="padding-left: .5rem;padding-right:.5rem;background-color: #97befc;">Setor</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $totalSetor = 0;
+                @endphp
+            
+                @foreach($data as $dt)
+                    @php
+                        $totalSetor += $dt['setor'];
+                    @endphp
+                    <tr style="font-size:.7rem">
+                        <td style="text-align:center;">{{ $loop->iteration }}</td>
+                        <td>{{ date('d/m/Y', strtotime($dt['tanggal'])) }}</td>
+                        <td style="text-align:center;">{{ $dt['no_transaksi'] }}</td>
+                        <td style="text-align:center;">{{ ucwords($dt['status']) }}</td>
+                        <td style="text-align:center;">
+                            {{ ($dt['armada']['nopol'] ?? $dt['nopol_subkon']) }} /
+                            {{ ($dt['sopir']['nama'] ?? $dt['sopir_subkon']) }}
+                        </td>
+                        <td>{{ ($dt['penyewa']['nama_perusahaan'] ?? $dt['subkon']['nama_perusahaan']) ?? '' }}</td>
+                        <td>{{ $dt['muatan'] }}</td>
+                        <td>{{ $dt['asal'] }} - {{ $dt['tujuan'] }}</td>
+                        <td>{{ rupiah($dt['setor']) }}</td>
+                    </tr>
+                @endforeach
+            
+                {{-- total row --}}
+                <tr style="font-size:.7rem">
+                    <td colspan="8" style="text-align:center;">Total Setor</td>
+                    <td>{{ rupiah($totalSetor) }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
--- resources/views/generate/pdf/v2/kas-harian.blade.php
+++ resources/views/generate/pdf/v2/kas-harian.blade.php
@@ -1,4 +1,8 @@
 @extends('generate.pdf.layouts.main', ['title' => $filename])
+@php
+    $totalPositive = 0;
+    $totalNegative = 0;
+@endphp
 @section('container')
     @include('generate.pdf.v2.style')
     <div class="container" style="border: 0">
@@ -44,17 +48,44 @@
                     </tr>
                 @endif
                 @foreach ($data as $index => $detail)
+                    @php
+                        $isNegative = in_array($detail['jenis_transaksi']->value, ['uang_jalan', 'pengeluaran']);
+                        $value = $detail['nominal'];
+                        if ($isNegative) {
+                            $totalNegative += $value;
+                        } else {
+                            $totalPositive += $value;
+                        }
+                    @endphp
                     <tr>
                         <td>{{ format_date($detail['tanggal_pembayaran']) }}</td>
                         <td>{{ $detail['transaksi_order'] ? $detail['transaksi_order']['no_transaksi'] : '-' }}</td>
                         <td>{{ $detail['jenis_transaksi']->value }}</td>
-                        @if ($detail['jenis_transaksi']->value == 'uang_jalan' || $detail['jenis_transaksi']->value == 'pengeluaran')
-                            <td style="color:red">{{ rupiah($detail['nominal'] * -1) }}</td>
+                        @if ($isNegative)
+                            <td style="color:red">{{ rupiah($value * -1) }}</td>
                         @else
-                            <td>{{ rupiah($detail['nominal']) }}</td>
+                            <td>{{ rupiah($value) }}</td>
                         @endif
                     </tr>
                 @endforeach
+                @php
+                    $totalAll = $totalPositive - $totalNegative;
+                @endphp
+                <tr>
+                    <td colspan="2"><strong>Totals</strong></td>
+                    <td><strong>Positive:</strong></td>
+                    <td>{{ rupiah($totalPositive) }}</td>
+                </tr>
+                <tr>
+                    <td colspan="2"></td>
+                    <td><strong>Negative:</strong></td>
+                    <td style="color:red">{{ rupiah($totalNegative * -1) }}</td>
+                </tr>
+                <tr>
+                    <td colspan="2"></td>
+                    <td><strong>Net Total:</strong></td>
+                    <td>{{ rupiah($totalAll) }}</td>
+                </tr>
             </tbody>
         </table>
     </div>
