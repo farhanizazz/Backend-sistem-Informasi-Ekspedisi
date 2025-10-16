5e977eec177c92115c4d42cff71d4e5b91a949d1
Farhan izzaz <farhanizzaz@gmail.com>
2025-10-16 19:10:31 +0700

    changed hutang customer and hutang subkon layout

--- resources/views/generate/pdf/v2/hutang-customer.blade.php
+++ resources/views/generate/pdf/v2/hutang-customer.blade.php
@@ -5,11 +5,11 @@
         <h3 style="text-align: center">{{ $filename }}</h3>
         <table class="table bordered">
             <tbody>
-                @if($customer)
-                <tr>
-                    <td><strong>Pelanggan</strong></td>
-                    <td>{{ $customer->nama_perusahaan }}</td>
-                </tr>
+                @if ($customer)
+                    <tr>
+                        <td><strong>Pelanggan</strong></td>
+                        <td>{{ $customer->nama_perusahaan }}</td>
+                    </tr>
                 @endif
                 {{-- <tr>
                     <td><strong>Total</strong> Hutang Realtime</td>
@@ -31,74 +31,52 @@
         </table>
     </div>
 
-    @foreach ($orders as $order)
-        <div class="container">
-            <div class="header">
-                <table class="table">
-                    <tbody>
-                        <tr>
-                            <td style="width: 50%"><strong>Tanggal</strong>: {{ format_date($order['tanggal']) }}</td>
-
-                            <td style="width: 50%"><strong>Asal/Tujuan</strong>: {{ $order['asal'] }} /
-                                {{ $order['tujuan'] }}</td>
-                        </tr>
-                        <tr>
-                            <td style="width: 50%"><strong>No Transaksi</strong>: {{ $order['no_transaksi'] }}</td>
-                            <td style="width: 50%"><strong>No Polisi/Sopir</strong>:
-                                {{ $order['nopol'] }} / {{ $order['sopir'] }}</td>
-                        </tr>
-                        <tr>
-                            <td style="width: 50%"><strong>Penyewa/muat</strong>: {{ $order['penyewa'] }} /
-                                {{ $order['muatan'] }}</td>
-                            {{-- <td><strong>Potongan THR</strong>: {{ rupiah($order['pot_thr']) }}</td> --}}
-                        </tr>
-                    </tbody>
-                </table>
-
-                <table class="table bordered" style="margin-top: 1rem;margin-bottom:1rem">
-                    <tbody>
-                        <tr>
-                            <td><strong>Total Tagihan</strong></td>
-                            <td> {{ rupiah($order['harga_order']) }}</td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <strong>Rincian Pembayaran</strong>
-                            </td>
-                            <td></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <table class="table table-text-left bordered">
-                <thead>
-                    <tr>
-                        <th>Tgl Bayar</th>
-                        <th>Keterangan</th>
-                        <th>Nominal</th>
-                        <th>Sisa Tagihan</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @if (count($order['rincian']) == 0)
-                        <tr>
-                            <td style="text-align: center" colspan="3">Rincian tidak tersedia</td>
-                            <td rowspan="{{ count($order['rincian']) }}">{{ rupiah($order['sisa_tagihan']) }}</td>
-                        </tr>
-                    @endif
-                    @foreach ($order['rincian'] as $index => $detail)
-                        <tr>
-                            <td>{{ format_date($detail['tanggal']) }}</td>
-                            <td>{{ $detail['keterangan'] }}</td>
-                            <td>{{ rupiah($detail['nominal']) }}</td>
-                            @if ($index == 0)
-                                <td rowspan="{{ count($order['rincian']) }}">{{ rupiah($order['sisa_tagihan']) }}</td>
-                            @endif
-                        </tr>
-                    @endforeach
-                </tbody>
+    <div class="container">
+        <div class="header">
+            <table class="table">
             </table>
         </div>
-        <div class="page_break"></div>
-    @endforeach
+        <table class="table table-text-left bordered">
+            <thead>
+                <tr>
+                    <th>Tgl Bayar / Nomor Transaksi</th>
+                    <th>Nopol / Sopir / Muatan</th>
+                    <th>Asal / Tujuan</th>
+                    <th>Harga Order</th>
+                    <th>Biaya Tambah / Kurang</th>
+                    <th>PPH / PPN</th>
+                    <th>Sisa Tagihan</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($orders as $order)
+                <tr>
+                    <td>
+                        {{ format_date($order['tanggal']) }} / {{ $order['no_transaksi'] }}
+                    </td>
+                    <td>
+                        {{ $order['nopol'] }} / {{ $order['sopir'] }} / {{ $order['muatan'] }}
+                    </td>
+                    <td>
+                        {{ $order['asal'] }} / {{ $order['tujuan'] }}
+                    </td>
+                    <td>
+                        {{ rupiah($order['harga_order']) }}
+                    </td>
+                    <td>
+                        {{ rupiah($order['biaya_tambah_kurang']) }}
+                    </td>
+                    <td>
+                        {{ rupiah($order['pph']) }}
+                    </td>
+                    <td>
+                        {{ rupiah($order['sisa_tagihan']) }}
+                    </td>
+                    
+                    
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
--- resources/views/generate/pdf/v2/hutang-subkon.blade.php
+++ resources/views/generate/pdf/v2/hutang-subkon.blade.php
@@ -25,82 +25,52 @@
         </table>
     </div>
 
-    @foreach ($orders as $order)
-        <div class="container">
-            <div class="header">
-                <table class="table">
-                    <tbody>
-                        <tr>
-                            <td style="width: 50%"><strong>Tanggal</strong>: {{ format_date($order['tanggal']) }}</td>
-                            {{-- <td><strong>Uang Jalan</strong>: {{ rupiah($order['uang_jalan']) }}</td> --}}
-                        </tr>
-                        <tr>
-                            <td style="width: 50%"><strong>No Transaksi</strong>: {{ $order['no_transaksi'] }}</td>
-                            {{-- <td><strong>Biaya Tambah / Kurang</strong>: {{ rupiah($order['biaya_tambahan']) }} /
-                                {{ rupiah($order['biaya_kurang']) }}</td> --}}
-                        </tr>
-                        <tr>
-                            <td style="width: 50%"><strong>Penyewa/muat</strong>: {{ $order['penyewa'] }} /
-                                {{ $order['muatan'] }}</td>
-                            {{-- <td><strong>Potongan THR</strong>: {{ rupiah($order['pot_thr']) }}</td> --}}
-                        </tr>
-                        <tr>
-                            <td style="width: 50%"><strong>Asal/Tujuan</strong>: {{ $order['asal'] }} /
-                                {{ $order['tujuan'] }}</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td style="width: 50%"><strong>No Polisi/Sopir</strong>:
-                                {{ $order['nopol'] }} / {{ $order['sopir'] }}</td>
-                            <td></td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                <table class="table bordered bordering-bottom" style="margin-top: 1rem;margin-bottom:1rem">
-                    <tbody>
-                        <tr>
-                            <td><strong>Total Tagihan</strong></td>
-                            <td> {{ rupiah($order['harga_jual']) }}</td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <strong>Rincian Pembayaran</strong>
-                            </td>
-                            <td></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <table class="table bordered table-text-left">
-                <thead>
-                    <tr>
-                        <th>Tgl Bayar</th>
-                        <th>Keterangan</th>
-                        <th>Nominal</th>
-                        <th>Sisa Hutang</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @if (count($order['rincian']) == 0)
-                        <tr>
-                            <td style="text-align: center" colspan="3">Rincian tidak tersedia</td>
-                            <td rowspan="{{ count($order['rincian']) }}">{{ rupiah($order['sisa_hutang']) }}</td>
-                        </tr>
-                    @endif
-                    @foreach ($order['rincian'] as $index => $detail)
-                        <tr>
-                            <td>{{ format_date($detail['tanggal']) }}</td>
-                            <td>{{ $detail['keterangan'] }}</td>
-                            <td>{{ rupiah($detail['nominal']) }}</td>
-                            @if ($index == 0)
-                                <td rowspan="{{ count($order['rincian']) }}">{{ rupiah($order['sisa_hutang']) }}</td>
-                            @endif
-                        </tr>
-                    @endforeach
-                </tbody>
+    <div class="container">
+        <div class="header">
+            <table class="table">
             </table>
         </div>
-        <div class="page_break"></div>
-    @endforeach
+        <table class="table table-text-left bordered">
+            <thead>
+                <tr>
+                    <th>Tgl Bayar / Nomor Transaksi</th>
+                    <th>Nopol / Sopir / Muatan</th>
+                    <th>Asal / Tujuan</th>
+                    <th>Harga Order</th>
+                    <th>Biaya Tambah / Kurang</th>
+                    <th>PPH / PPN</th>
+                    <th>Sisa Tagihan</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($orders as $order)
+                <tr>
+                    <td>
+                        {{ format_date($order['tanggal']) }} / {{ $order['no_transaksi'] }}
+                    </td>
+                    <td>
+                        {{ $order['nopol'] }} / {{ $order['sopir'] }} / {{ $order['muatan'] }}
+                    </td>
+                    <td>
+                        {{ $order['asal'] }} / {{ $order['tujuan'] }}
+                    </td>
+                    <td>
+                        {{ rupiah($order['harga_order']) }}
+                    </td>
+                    <td>
+                        {{ rupiah($order['biaya_tambah_kurang']) }}
+                    </td>
+                    <td>
+                        {{ rupiah($order['pph']) }}
+                    </td>
+                    <td>
+                        {{ rupiah($order['sisa_hutang']) }}
+                    </td>
+                    
+                    
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
